cee5833e29a11cf0ff5c2a525fe6223afe304d81
王震 <lavalike@yeah.net>
2019-11-11 20:14:56 +0800

    集成biz-core

--- app/src/main/java/cn/com/zjol/quick_login/QuickLoginApplication.java
+++ app/src/main/java/cn/com/zjol/quick_login/QuickLoginApplication.java
@@ -2,6 +2,12 @@ package cn.com.zjol.quick_login;
 
 import android.app.Application;
 
+import com.aliya.uimode.UiModeManager;
+import com.zjrb.core.utils.AppUtils;
+import com.zjrb.core.utils.UIUtils;
+
+import cn.com.zjol.biz.core.network.DailyNetworkManager;
+
 /**
  * QuickLoginApplication
  * Created by wangzhen on 2019-11-11.
@@ -10,6 +16,10 @@ public class QuickLoginApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        UIUtils.init(this);
+        AppUtils.setChannel("AppStore");
+        UiModeManager.init(this, null);
+        DailyNetworkManager.init(this);
         OneClickLogin.init(this, "123");
     }
 }
